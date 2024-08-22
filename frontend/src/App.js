e8af67bf5b705d9a7f6155428caf340d59fd2066
marshall daniel <marshalldanielgalloguiza@gmail.com>
2024-08-22 16:26:39 -0500

    cambios en la vista administrativa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,10 @@ import Bienvenida from "./paginas/auth/bienvenida";
 import Register from "./paginas/auth/registro_clientes";
 import Nosotros from "./paginas/auth/nosotros";
 import Contacto from "./paginas/auth/contactanos";
+import UsuariosAdmin from "./paginas/auth/usuarios_admin";
+import Productos from "./paginas/productos";
+import VentasAdmin from "./paginas/auth/ventas_admin";
+import DomicilioAdmin from "./paginas/auth/domicilios_admin";
 function App() {
   return (
     <Fragment>
@@ -14,6 +18,10 @@ function App() {
           <Route path='/registro_clientes.js' exact element={<Register />}/>
           <Route path='/nosotros.js' exact element={<Nosotros />}/>
           <Route path='/contactanos.js' exact element={<Contacto />}/>  
+          <Route path='/usuarios_admin.js' exact element={<UsuariosAdmin />}/>
+          <Route path='/productos.js' exact element={<Productos />}/>
+          <Route path='/ventas_admin.js' exact element={<VentasAdmin />}/>
+          <Route path='/domicilios_admin.js' exact element={<DomicilioAdmin />}/>
         </Routes>
       </Router>
     </Fragment>
